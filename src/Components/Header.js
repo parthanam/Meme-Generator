db0c83fb93b89fdca686c6445094baf943e42eb8
parthanam <parthnautiyal1016@gmail.com>
2023-06-19 01:23:01 +0530

    created Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Header()
+{
+    return(
+        <div className="header">
+            <img src="./frog-meme.png" alt="there"></img>
+            <h1>Meme Generator</h1>
+            <h4>React Project 3</h4>
+        </div>
+    )
+}
